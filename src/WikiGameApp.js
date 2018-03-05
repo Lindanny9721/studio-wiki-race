27042613e510841cfccf01e8c8ae4a2c3942ef77
Daniel Lin <lindanny9721@gmail.com>
2018-03-05 23:29:08 +0000

    Summary added

--- src/WikiGameApp.js
+++ src/WikiGameApp.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-
+import GameView from './GameView.js';
 export default class WikiGameApp extends Component {
   render() {
     // Properties (data) provided by WikiGameHistory.
     var isComplete = this.props.isComplete;
     var titles = this.props.titles;
     var targetTitle = this.props.targetTitle;
+    var currentTitle = this.props.currentTitle;
 
     // Properties (functions) provided by WikiGameHistory.
     var addTitle = this.props.addTitle;
     var setCurrentTitle = this.props.setCurrentTitle;
-
     return (
       <div>
-        TODO: Add links to new components here.
+       <GameView title={currentTitle} />
       </div>
     );
   }
